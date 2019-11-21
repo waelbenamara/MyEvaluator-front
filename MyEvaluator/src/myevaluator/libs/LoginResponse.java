107fa89e37a858ad6f2ae18d09ab05f15f42a9a8
waelbenamara <wael.benamara@medtech.tn>
2019-11-21 15:18:13 +0100

    Commit

---
+++ MyEvaluator/src/myevaluator/libs/LoginResponse.java
@@ -0,0 +1,27 @@
+package myevaluator.libs;
+
+public class LoginResponse {
+  
+  private String status;
+  
+  private String type;
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  
+
+}
---
+++ MyEvaluator/src/myevaluator/models/Credentials.java
@@ -0,0 +1,27 @@
+package myevaluator.models;
+
+public class Credentials {
+  
+  private String email;
+  private String password;
+  
+  
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public Credentials(String email, String password) {
+    super();
+    this.email = email;
+    this.password = password;
+  }
+
+}
